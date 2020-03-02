101dfe67ac036f9bddf64c8dff94c4f50a7f8310
Lisa Gren <lisa.c.gren@gmail.com>
2020-03-02 09:53:54 +0100

    sample images

--- main.m
+++ main.m
@@ -24,6 +24,21 @@ imshow(legoGen)
 subplot(1,3,3)
 imshow(legoSpec)
 
+%% More legofied images
+
+for var = 1:6
+    im  = imread(strcat('image', int2str(var), '.png'));
+    [legoImg, legoGen, legoSpec] = legofy(im);
+    figure
+    subplot(2,2,1)
+    imshow(im)
+    subplot(2,2,2)
+    imshow(legoImg)
+    subplot(2,2,3)
+    imshow(legoGen)
+    subplot(2,2,4)
+    imshow(legoSpec)
+end
 
 
 
